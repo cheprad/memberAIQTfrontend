0e61a2ab5709dd73a0d843c483301c355dd2836e
cheprad <68316487+cheprad@users.noreply.github.com>
2022-09-05 12:59:07 +0700

    init

---
+++ index.php
@@ -0,0 +1,81 @@
+
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<link rel="icon" href="http://aiquanttech.com/images/aiqt_logo_small.jpg" type="image/jpg" />
+    <title>Ai Quant Tech</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="css/plugins/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+	<script src="./js/jquery.min.js"></script>	
+	
+		<!-- Bootstrap CSS -->
+		<title>AI Quant Tech Login</title>
+		<style>
+		body {
+		background-image: url('http://member.aiquanttech.com/bg.jpg');
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-size: 100% 100%;
+		}
+		</style>
+	</head>
+	<body>
+		<div class="container" style="margin-top: 20px;">
+			<div class="row">
+				<div class="col-sm-8"></div>
+				<div class="col-sm-4">
+
+					<form action="login.php" method="post">
+						<span style="color:yellow;">Login</span>
+						<input type="text" name="aiqtusername" class="form-control" required placeholder="E-mail">
+						<span style="color:yellow;">Password</span>
+						<input type="password" name="aiqtpassword" class="form-control" required placeholder="Password">
+						<br>
+						<input type="button" class="btn btn-warning" onclick="location.href='regnewmember.html';" value="Register New User" />
+						&nbsp;&nbsp;&nbsp;&nbsp;
+						<button type="submit" class="btn btn-success" >Login</button> 
+						<input type="hidden" name="mk" value="86513941433483774761999956892451741144186">
+					</form>
+					
+						
+
+					<BR>
+					<div id="fgpass"><a href="forgetpasswd.php" target="fgw"><font color="white">ลืมรหัสผ่าน (Forget Password)</font></a></div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container" style="margin-top: 350px;">
+			<div class="row">
+				<div class="col-sm-8"></div>
+				<div class="col-sm-4">
+					<h4 style="color:yellow;">Contact Us (Thailand)</h4>
+					<div style="color:yellow;">Email :: info@aiquanttech.com</div>
+					<div style="color:yellow;">Tel :: 095-824-4526</div>
+				</div>
+			</div>
+		</div>
+		
+
+
+	</body>
+</html>
